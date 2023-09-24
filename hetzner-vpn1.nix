04c82ccd9975d34d05df43b50b3dbc7132d66a96
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2023-09-24 14:59:21 +0200

    fix vpn1 not building

--- hetzner-vpn1.nix
+++ hetzner-vpn1.nix
@@ -7,7 +7,10 @@
   ];
 
   config = {
-    my.server.enable = true;
+    my = {
+      enabledUsers = [];
+      server.enable = true;
+    };
 
     users.users.root.openssh.authorizedKeys.keys = [
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICdYqY3Y1/f1bsAi5Qfyr/UWuX9ixu96IeAlhoQaJkbf''
