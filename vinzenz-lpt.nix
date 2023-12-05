abd0d146eabf362b6835e39f621fd4ed97f77bdc
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2023-12-05 15:00:02 +0100

    allow pc2 ssh key on all hosts

--- vinzenz-lpt.nix
+++ vinzenz-lpt.nix
@@ -17,9 +17,9 @@
       };
     };
 
-
     users.users.vinzenz.openssh.authorizedKeys.keys = [
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCJUpbpB3KEKVoKWsKoar9J4RNah8gmQoSH6jQEw5dY vinzenz-pixel-JuiceSSH''
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIO1CRn4yYTL4XUdCebE8Z4ZeuMujBjorTdWifg911EOv vinzenz-pc2 home roaming''
     ];
   };
 }
