a7fedb9e8abccc8aa6d511b8ab18fc126145695e
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2023-12-25 12:06:58 +0100

    script template

--- modules/users/home-shared-modules.nix
+++ modules/users/home-shared-modules.nix
@@ -43,6 +43,15 @@
   }
   # Templates
   {
-    home.file."Templates/Empty file".text = "";
+    home.file = {
+      "Templates/Empty file".text = "";
+      "Templates/Empty bash script".text = ''
+        #!/usr/bin/env bash
+        # abort on error, undefined variables
+        set -eu
+        # print commands before execution
+        set -x
+      '';
+    };
   }
 ]
