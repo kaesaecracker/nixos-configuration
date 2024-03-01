07a9c7a7f7c3c4037ffcac4fefb6a321796aa3cd
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-03-01 17:39:37 +0100

    add new key for lpt2

--- vinzenz-pc2.nix
+++ vinzenz-pc2.nix
@@ -26,6 +26,7 @@
     users.users.vinzenz.openssh.authorizedKeys.keys = [
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINrY6tcgnoC/xbgL7vxSjddEY9MBxRXe9n2cAHt88/TT home roaming''
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFCJUpbpB3KEKVoKWsKoar9J4RNah8gmQoSH6jQEw5dY vinzenz-pixel-JuiceSSH''
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDNpLDmctyqGpow/ElQvdhY4BLBPS/sigDJ1QEcC7wC vinzenz-lpt2-roaming''
     ];
   };
 }
