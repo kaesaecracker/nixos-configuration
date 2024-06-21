d355e877cd4cc20a6d5b97b1c4b5d85df93ce823
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-06-21 17:12:04 +0200

    change vscode-server, WOL

--- vinzenz-pc2.nix
+++ vinzenz-pc2.nix
@@ -39,11 +39,19 @@
       port = 8542;
       host = "100.125.93.127"; # tailscale
       withoutConnectionToken = true;
-      extraPackages = with pkgs; [nodejs gitFull gh];
+      extraPackages = with pkgs; [nodejs gitFull gh direnv];
     };
 
-    networking.firewall = {
-      allowedTCPPorts = [8542 8543 8544];
+    virtualisation.podman = {
+      enable = true;
+    };
+
+    networking = {
+      firewall = {
+        allowedTCPPorts = [8542 8543 8544 80];
+      };
+
+      interfaces.eno1.wakeOnLan.enable = true;
     };
   };
 }
