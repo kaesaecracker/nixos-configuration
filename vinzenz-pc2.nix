9b8947a1a7170c717a9f07ca40f2b3a980aaeefe
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-06-21 17:11:13 +0200

    add key to pc2

--- vinzenz-pc2.nix
+++ vinzenz-pc2.nix
@@ -29,6 +29,10 @@
       ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPDNpLDmctyqGpow/ElQvdhY4BLBPS/sigDJ1QEcC7wC vinzenz-lpt2-roaming''
     ];
 
+    users.users.ronja.openssh.authorizedKeys.keys = [
+      ''ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIALWKm+d6KL6Vl3grPOcGouiNTkvdhXuWJmcrdEBY2nw ssh-host-key''
+    ];
+
     services.openvscode-server = {
       enable = true;
       telemetryLevel = "off";
