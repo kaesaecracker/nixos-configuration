160229278171b46bec07ac1806ba9b766a91e95f
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-10-26 00:16:05 +0200

    enable experimental features

--- common/nixpkgs.nix
+++ common/nixpkgs.nix
@@ -18,6 +18,7 @@
       settings = {
         substituters = ["https://nix-community.cachix.org" "https://cache.nixos.org/"];
         trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
+        experimental-features = ["nix-command" "flakes"];
       };
       gc = {
         automatic = true;
