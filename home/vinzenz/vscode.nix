f5b0ca48a7b18100ba96473b4cb523a298520405
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-12-14 11:18:20 +0100

    tweak vscode settings, remove git graph as it is not maintained anymore

--- home/vinzenz/vscode.nix
+++ home/vinzenz/vscode.nix
@@ -11,7 +11,6 @@
     yzhang.markdown-all-in-one
     redhat.vscode-yaml
     pkief.material-icon-theme
-    mhutchie.git-graph
     rust-lang.rust-analyzer
     tamasfe.even-better-toml
     llvm-vs-code-extensions.vscode-clangd
@@ -21,25 +20,34 @@
     ms-vscode-remote.remote-ssh
   ];
   userSettings = {
-    "git.autofetch" = true;
-    "update.mode" = "none";
+    "files.autoSave" = "afterDelay";
+    "files.autoSaveWhenNoErrors" = true;
+    "files.autoSaveWorkspaceFilesOnly" = true;
+
     "editor.fontFamily" = "'Fira Code', 'Droid Sans Mono', 'monospace', monospace";
     "editor.fontLigatures" = true;
     "editor.formatOnSave" = true;
     "editor.formatOnSaveMode" = "modificationsIfAvailable";
     "editor.minimap.autohide" = true;
+
+    "workbench.startupEditor" = "readme";
+    "workbench.enableExperiments" = false;
+    "workbench.iconTheme" = "material-icon-theme";
+
+    "update.mode" = "none";
+    "extensions.autoUpdate" = false;
+    "extensions.autoCheckUpdates" = false;
+
+    "telemetry.telemetryLevel" = "off";
+    "redhat.telemetry.enabled" = false;
+
+    "git.autofetch" = true;
     "diffEditor.diffAlgorithm" = "advanced";
     "explorer.excludeGitIgnore" = true;
     "markdown.extension.tableFormatter.normalizeIndentation" = true;
     "markdown.extension.toc.orderedList" = false;
-    "telemetry.telemetryLevel" = "off";
-    "redhat.telemetry.enabled" = false;
-    "workbench.startupEditor" = "readme";
-    "workbench.enableExperiments" = false;
-    "workbench.iconTheme" = "material-icon-theme";
     "rust-analyzer.checkOnSave.command" = "clippy";
-    "extensions.autoUpdate" = false;
-    "extensions.autoCheckUpdates" = false;
+
     "\[makefile\]" = {
       "editor.insertSpaces" = false;
       "editor.detectIndentation" = false;
