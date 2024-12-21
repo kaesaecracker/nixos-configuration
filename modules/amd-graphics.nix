906a264118728cd3868216398e15a2669f1c18d6
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2024-12-21 14:13:01 +0100

    disable amdvlk because of gnome bug, update config

--- modules/amd-graphics.nix
+++ modules/amd-graphics.nix
@@ -1,12 +1,19 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   config = {
     boot.kernelModules = [ "amdgpu" ];
     services.xserver.videoDrivers = [ "amdgpu" ];
 
-    hardware.graphics = {
-      extraPackages = with pkgs; [ amdvlk ];
-      extraPackages32 = with pkgs; [ driversi686Linux.amdvlk ];
+    hardware = {
+      graphics.enable = true;
+      amdgpu = {
+        opencl.enable = true;
+        amdvlk = {
+          # TODO: this creates black borders around GNOME apps
+          # enable = true;
+          support32Bit.enable = config.hardware.graphics.enable32Bit;
+        };
+      };
     };
 
     environment.systemPackages = with pkgs; [ nvtopPackages.amd ];
