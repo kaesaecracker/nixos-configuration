8b48c54f48f3edd3c588b19a1a15c5d12b1de6c2
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2025-01-02 17:56:33 +0100

    enable fractional scaling for gnome

--- modules/gnome.nix
+++ modules/gnome.nix
@@ -4,7 +4,14 @@
     services = {
       xserver = {
         # Enable the GNOME Desktop Environment.
-        desktopManager.gnome.enable = true;
+        desktopManager.gnome = {
+          enable = true;
+          extraGSettingsOverridePackages = [ pkgs.mutter ];
+          extraGSettingsOverrides = ''
+            [org.gnome.mutter]
+            experimental-features=['scale-monitor-framebuffer']
+          '';
+        };
         displayManager.gdm.enable = true;
         excludePackages = with pkgs; [ xterm ];
       };
