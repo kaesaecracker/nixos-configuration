a620f86204722d88a2bfb314a4b3d42e151a58fb
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2025-03-15 10:25:44 +0100

    update zsh plugin list

--- home/vinzenz/zsh.nix
+++ home/vinzenz/zsh.nix
@@ -11,7 +11,7 @@
     my-apply = "sudo nixos-rebuild boot --flake";
     my-switch = "sudo nixos-rebuild switch --flake";
     my-update = "sudo nixos-rebuild boot --upgrade --flake";
-    my-pull = "git -C ~/Repos/nixos-configuration pull --rebase"; 
+    my-pull = "git -C ~/Repos/nixos-configuration pull --rebase";
     my-test = "sudo nixos-rebuild test";
     my-direnvallow = "echo \"use nix\" > .envrc && direnv allow";
     my-ip4 = "ip addr show | grep 192";
@@ -31,8 +31,12 @@
     plugins = [
       "git"
       "sudo"
-      "docker"
       "systemadmin"
+      "battery"
+      "dotnet"
+      "rust"
+      "tailscale"
+      "thefuck"
     ];
   };
 }
