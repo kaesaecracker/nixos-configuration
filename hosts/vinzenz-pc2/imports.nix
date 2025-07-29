e0f956950ada949b9b60617aaa40615067fc8c33
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2025-07-29 18:01:54 +0200

    disable niri and open port on pc2

--- hosts/vinzenz-pc2/imports.nix
+++ hosts/vinzenz-pc2/imports.nix
@@ -4,7 +4,7 @@
     ../../modules/gaming.nix
     ../../modules/printing.nix
     ../../modules/podman.nix
-    ../../modules/niri.nix
+    #../../modules/niri.nix
     ../../modules/desktop-environment.nix
     ../../modules/desktop-hardware.nix
 
