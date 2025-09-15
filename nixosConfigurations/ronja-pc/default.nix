9cf6333f4b0b470ba61661c714bc7eeb84c02f15
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2025-09-15 19:05:04 +0200

    merge default.nix and configuration.nix

--- nixosConfigurations/ronja-pc/default.nix
+++ nixosConfigurations/ronja-pc/default.nix
@@ -1,6 +1,32 @@
 {
+  config,
+  pkgs,
+  ...
+}:
+{
   imports = [
-    ./configuration.nix
     ./hardware.nix
   ];
+
+  config = {
+    # Configure keymap in X11
+    services.xserver.xkb = {
+      layout = "de";
+      variant = "";
+    };
+
+    # Configure console keymap
+    console.keyMap = "de";
+
+    # List packages installed in system profile. To search, run:
+    # $ nix search wget
+    environment.systemPackages = with pkgs; [
+      #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+      #  wget
+    ];
+
+    # Open ports in the firewall.
+    # networking.firewall.allowedTCPPorts = [ ... ];
+    # networking.firewall.allowedUDPPorts = [ ... ];};
+  };
 }
