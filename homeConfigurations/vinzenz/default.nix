edb4ffcd4a6425a9fce721568e91e332902f65a7
Vinzenz Schroeter <vinzenz.f.s@gmail.com>
2025-09-16 19:29:09 +0200

    merge default and configuration.nix

--- homeConfigurations/vinzenz/default.nix
+++ homeConfigurations/vinzenz/default.nix
@@ -1,6 +1,6 @@
+{ pkgs, ... }:
 {
   imports = [
-    ./configuration.nix
     ./editorconfig.nix
     ./fuzzel.nix
     ./git.nix
@@ -12,4 +12,78 @@
     ./waybar.nix
     ./zsh.nix
   ];
+
+  config = {
+    programs = {
+      home-manager.enable = true;
+      fzf.enable = true;
+      git-credential-oauth.enable = true;
+
+      direnv = {
+        enable = true;
+        nix-direnv.enable = true;
+        enableZshIntegration = true;
+      };
+
+      eza = {
+        enable = true;
+        git = true;
+        icons = "auto";
+        extraOptions = [
+          "--group-directories-first"
+          "--header"
+        ];
+      };
+
+      thefuck = {
+        enable = true;
+        enableZshIntegration = true;
+      };
+
+      chromium.enable = true;
+    };
+
+    home.packages = with pkgs; [
+      keepassxc
+
+      telegram-desktop
+      element-desktop
+
+      wireguard-tools
+      wirelesstools
+
+      kdiff3
+      jetbrains-toolbox
+
+      blanket
+      vlc
+
+      ptyxis
+
+      arduino
+      arduino-ide
+      arduino-cli
+
+      servicepoint-cli
+      servicepoint-simulator
+
+      icu
+
+      nextcloud-client
+
+      lutris
+
+      foliate
+
+      dconf2nix
+    ];
+
+    home.file = {
+      "policy.json" = {
+        target = ".config/containers/policy.json";
+        text = builtins.readFile ./.config/containers/policy.json;
+      };
+      "idea.properties".text = "idea.filewatcher.executable.path = ${pkgs.fsnotifier}/bin/fsnotifier";
+    };
+  };
 }
