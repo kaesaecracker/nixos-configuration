0182c37a0406eca4e4c40156a14579c2d76b91c2
müde <git@darkest.space>
2026-05-17 22:23:35 +0200

    nix flake update, enable hyperhive forge

--- nixosConfigurations/muede-lpt2/hyperhive.nix
+++ nixosConfigurations/muede-lpt2/hyperhive.nix
@@ -1,6 +1,10 @@
 { hyperhive, ... }:
 {
-  imports = [ hyperhive.nixosModules.hive-c0re ];
+  imports = [
+    hyperhive.nixosModules.hive-c0re
+    hyperhive.nixosModules.hive-forge
+  ];
 
   config.services.hive-c0re.enable = true;
+  config.services.hive-forge.enable = true;
 }
