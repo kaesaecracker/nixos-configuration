34923781020fb60f92c3ed4673d5254d210a6d25
müde <git@darkest.space>
2026-05-31 16:01:02 +0200

    forge: add storage volume

--- nixosConfigurations/hyperforge/hardware.nix
+++ nixosConfigurations/hyperforge/hardware.nix
@@ -10,5 +10,11 @@
       swapUuid = "737140f2-c2fd-4af9-9974-f05642f8d90e";
       ipv6Address = "2a01:4f8:c013:cbdd::1/64";
     };
+
+    fileSystems."/var/lib/forgejo" = {
+      device = "/dev/disk/by-uuid/da5efb9a-7dc2-4fa3-aeca-c9bf634054b0";
+      fsType = "ext4";
+      options = [ "discard" "nofail" "defaults" ];
+    };
   };
 }
